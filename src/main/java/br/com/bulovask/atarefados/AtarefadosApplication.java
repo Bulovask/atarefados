a5bfb67de2fae757367553cbb07cb47942586392
Bulovask <ageua75@gmail.com>
2024-10-29 20:39:25 -0300

    first commit

---
+++ src/main/java/br/com/bulovask/atarefados/AtarefadosApplication.java
@@ -0,0 +1,13 @@
+package br.com.bulovask.atarefados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtarefadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtarefadosApplication.class, args);
+	}
+
+}
