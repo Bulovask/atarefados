da5c4e3aa897aeefeb1321f738d698cbc1b77799
Carlitoshenrique <chqc@discente.ifpe.edu.br>
2024-11-19 21:45:41 -0300

    Update ComentarioTarefaController.java

--- src/main/java/Controller/ComentarioTarefaController.java
+++ src/main/java/Controller/ComentarioTarefaController.java
@@ -1,4 +1,10 @@
 package Controller;
 
+import Service.ComentarioTarefaService;
+import br.com.bulovask.atarefados.entidades.ComentarioTarefa;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ComentarioTarefaController {
+    @Autowired
+    private ComentarioTarefaService comentarioTarefaService;
 }
