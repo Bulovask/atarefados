829b955d8c044b77a18fed731deddc7c350c4cff
Carlitoshenrique <165968270+Carlitoshenrique@users.noreply.github.com>
2024-11-26 20:17:28 -0300

    testando

--- src/main/java/br/com/bulovask/atarefados/Controller/ListaController.java
+++ src/main/java/br/com/bulovask/atarefados/Controller/ListaController.java
@@ -1,4 +1,42 @@
 package br.com.bulovask.atarefados.Controller;
 
+import br.com.bulovask.atarefados.Service.ListaService;
+import br.com.bulovask.atarefados.entidades.Lista;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
 public class ListaController {
+
+    @Autowired
+    private ListaService listaService;
+
+    @PostMapping("/lista")
+    public Lista salvar(@RequestBody Lista lista){
+        return listaService.salvar(lista);
+    }
+
+    @GetMapping("/lista")
+    public Iterable<Lista> listarTodos (){
+        return listaService.listarTodos();
+    }
+
+    @GetMapping("/lista/{id}")
+    public Optional<Lista> buscarPorId(@PathVariable Long id){
+        return listaService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/lista/{id}")
+    public ResponseEntity deletar(@PathVariable Long id){
+        return listaService.deletar(id);
+    }
+
+    @PutMapping("/lista/{id}")
+    public Lista atualizar(@PathVariable Long id, @RequestBody Lista lista){
+        lista.setId(id);
+        return listaService.salvar(lista);
+    }
 }
--- src/main/java/br/com/bulovask/atarefados/Service/ListaService.java
+++ src/main/java/br/com/bulovask/atarefados/Service/ListaService.java
@@ -1,4 +1,40 @@
 package br.com.bulovask.atarefados.Service;
 
+import br.com.bulovask.atarefados.Repository.ListaRepository;
+import br.com.bulovask.atarefados.entidades.Lista;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ListaService {
+
+    @Autowired
+    private ListaRepository listaRepository;
+
+    public List<Lista> listarTodos() {
+        return (List<Lista>) listaRepository.findAll();
+    }
+
+    public Optional<Lista> buscarPorId(Long id) {
+        return listaRepository.findById(id);
+    }
+
+    public Lista salvar(Lista lista) {
+        return listaRepository.save(lista);
+    }
+
+    public Lista atualizar(Long id, Lista listaAtualizada) {
+        Optional<Lista> listaExistente = listaRepository.findById(id);
+        if (listaExistente.isPresent()) {
+            Lista lista = listaExistente.get();
+            lista.setUsuario(listaAtualizada.getUsuario());
+            lista.setProjeto(listaAtualizada.getProjeto());
+            return listaRepository.save(lista);
+        } else {
+            throw new RuntimeException("Item da Lista não encontrado com o ID: " + id);
+        }
+    }
+
+    public ResponseEntity deletar(Long id) {
+        listaRepository.deleteById(id);
+        return new ResponseEntity("{\"mensagem\":\"Removido com sucesso\"}", HttpStatus.OK);
+    }
 }
