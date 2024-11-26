2fb9466c4feda3b59eef4cb28d8dea393691d73b
Carlitoshenrique <165968270+Carlitoshenrique@users.noreply.github.com>
2024-11-26 19:45:31 -0300

    Update UsuarioController.java

--- src/main/java/br/com/bulovask/atarefados/Controller/UsuarioController.java
+++ src/main/java/br/com/bulovask/atarefados/Controller/UsuarioController.java
@@ -1,4 +1,27 @@
 package br.com.bulovask.atarefados.Controller;
 
+import br.com.bulovask.atarefados.Service.UsuarioService;
+import br.com.bulovask.atarefados.entidades.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class UsuarioController {
+
+
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping("/usuario")
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) {
+        return usuarioService.salvar(usuario);
+    }
 }
+
+
+
