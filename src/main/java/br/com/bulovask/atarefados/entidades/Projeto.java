c7cf02a883ee923cef9ccaedab78ea42e2c71db8
Bulovask <ageua75@gmail.com>
2024-11-26 21:50:17 -0300

    Update Projeto.java

--- src/main/java/br/com/bulovask/atarefados/entidades/Projeto.java
+++ src/main/java/br/com/bulovask/atarefados/entidades/Projeto.java
@@ -12,6 +12,7 @@ public class Projeto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @ManyToOne
     private Usuario usuarioDono;
 
     @OneToMany
