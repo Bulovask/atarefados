6cd443437777d299d46f3187693fb3d338cc61d2
Carlitoshenrique <165968270+Carlitoshenrique@users.noreply.github.com>
2024-11-26 21:51:51 -0300

    frff

--- src/main/java/br/com/bulovask/atarefados/Controller/CategoriaTarefaController.java
+++ src/main/java/br/com/bulovask/atarefados/Controller/CategoriaTarefaController.java
@@ -1,4 +1,40 @@
 package br.com.bulovask.atarefados.Controller;
 
+import br.com.bulovask.atarefados.Service.CategoriaService;
+import br.com.bulovask.atarefados.entidades.Categoria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class CategoriaTarefaController {
+
+    @Autowired
+    private CategoriaTarefaService categoriaTarefaService;
+
+    @PostMapping("/CategoriaTarefa")
+    public CategoriaTarefa salvar(@RequestBody CategoriaTarefa categoriaTarefa){
+        return categoriaTarefaService.salvar(categoriaTarefa).getBody();
+    }
+
+    @GetMapping("/CategoriaTarefa")
+    public Iterable<Categoria> listarTodos (){
+        return categoriaTarefaService.listarTodos();
+    }
+
+    @GetMapping("/CategoriaTarefa/{id}")
+    public ResponseEntity<Categoria> buscarPorId(@PathVariable Long id){
+        return categoriaTarefaService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/CategoriaTarefa/{id}")
+    public ResponseEntity deletar(@PathVariable Long id){
+        return categoriaTarefaService.deletar(id);
+    }
+
+    @PutMapping("/CategoriaTarefa/{id}")
+    public CategoriaTarefa atualizar(@PathVariable Long id, @RequestBody CategoriaTarefa categoriaTarefa){
+        categoria.setId(id);
+        return categoriaTarefaService.salvar(categoriaTarefa).getBody();
+    }
 }
