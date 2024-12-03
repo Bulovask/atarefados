5eeba5e9a62e1e9e788ca1aad8a9d304d32966cf
Carlitoshenrique <165968270+Carlitoshenrique@users.noreply.github.com>
2024-12-03 20:05:47 -0300

    Update UsuarioProjetoController.java

--- src/main/java/br/com/bulovask/atarefados/Controller/UsuarioProjetoController.java
+++ src/main/java/br/com/bulovask/atarefados/Controller/UsuarioProjetoController.java
@@ -1,19 +1,33 @@
 package br.com.bulovask.atarefados.Controller;
 
 import br.com.bulovask.atarefados.Service.UsuarioProjetoService;
+import br.com.bulovask.atarefados.Service.UsuarioService;
+import br.com.bulovask.atarefados.entidades.Anexo;
 import br.com.bulovask.atarefados.entidades.Usuario;
 import br.com.bulovask.atarefados.entidades.UsuarioProjeto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
 public class UsuarioProjetoController {
 
 
 
     @Autowired
     private UsuarioProjetoService usuarioProjetoService;
+
+    @GetMapping("/usuarioProjeto")
+    public Iterable<UsuarioProjeto> listarTodos(@RequestBody UsuarioProjeto usuarioProjeto) {
+        List<UsuarioProjeto> usuarioProjetos = (List<UsuarioProjeto>) usuarioProjetoService.listarTodos();
+        return usuarioProjetos;
+    }
+    @GetMapping("/usuarioProjeto/{id}")
+    public ResponseEntity<UsuarioProjeto> buscarPorId(@PathVariable Long id){
+        return usuarioProjetoService.buscarPorId(id);
+    }
 
     @PostMapping("/usuarioProjeto")
     public ResponseEntity<UsuarioProjeto> salvar(@RequestBody UsuarioProjeto usuarioProjeto) {
