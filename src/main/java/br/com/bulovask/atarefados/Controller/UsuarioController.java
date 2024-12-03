568a6493d800d592e8cb5c1348c13c70aad77fce
Carlitoshenrique <165968270+Carlitoshenrique@users.noreply.github.com>
2024-12-03 19:57:29 -0300

    Update UsuarioController.java

--- src/main/java/br/com/bulovask/atarefados/Controller/UsuarioController.java
+++ src/main/java/br/com/bulovask/atarefados/Controller/UsuarioController.java
@@ -4,9 +4,12 @@ import br.com.bulovask.atarefados.Service.UsuarioService;
 import br.com.bulovask.atarefados.entidades.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -16,6 +19,12 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @GetMapping("/usuario")
+    public Iterable<Usuario> listarTodos(@RequestBody Usuario usuario) {
+        List<Usuario> usuarios = (List<Usuario>) usuarioService.listarTodos();
+        return usuarios;
+    }
 
     @PostMapping("/usuario")
     public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) {
