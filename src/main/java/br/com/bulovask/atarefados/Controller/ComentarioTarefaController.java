eb7fc0e0218a9df98977b60eb617d06a5ffba8f1
Carlitoshenrique <165968270+Carlitoshenrique@users.noreply.github.com>
2024-12-03 20:21:14 -0300

    Update ComentarioTarefaController.java

--- src/main/java/br/com/bulovask/atarefados/Controller/ComentarioTarefaController.java
+++ src/main/java/br/com/bulovask/atarefados/Controller/ComentarioTarefaController.java
@@ -14,27 +14,27 @@ public class ComentarioTarefaController {
     @Autowired
     private ComentarioTarefaService comentarioTarefaService;
 
-    @PostMapping("/comentarioTarefa")
+    @PostMapping("/comentario-tarefa")
     public ComentarioTarefa salvar(@RequestBody ComentarioTarefa comentarioTarefa){
         return ComentarioTarefaService.salvar(comentarioTarefa).getBody();
     }
 
-    @GetMapping("/comentarioTarefa")
+    @GetMapping("/comentario-tarefa")
     public Iterable<ComentarioTarefa> listarTodos (){
         return comentarioTarefaService.listarTodos();
     }
 
-    @GetMapping("/comentarioTarefa/{id}")
+    @GetMapping("/comentario-tarefa/{id}")
     public ResponseEntity<ComentarioTarefa> buscarPorId(@PathVariable Long id){
         return comentarioTarefaService.buscarPorId(id);
     }
 
-    @DeleteMapping("/comentrioTarefa/{id}")
+    @DeleteMapping("/comentario-tarefa/{id}")
     public ResponseEntity deletar(@PathVariable Long id){
         return comentarioTarefaService.deletar(id);
     }
 
-    @PutMapping("/comentarioTarefa/{id}")
+    @PutMapping("/comentario-tarefa/{id}")
     public ComentarioTarefa atualizar(@PathVariable Long id, @RequestBody ComentarioTarefa comentarioTarefa){
         comentarioTarefa.setId(id);
         return comentarioTarefaService.salvar(comentarioTarefa).getBody();
