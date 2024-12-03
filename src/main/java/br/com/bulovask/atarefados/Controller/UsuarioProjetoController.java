6dd9d9cadb70dd667a57bbea2ae2d7ced199e862
Carlitoshenrique <165968270+Carlitoshenrique@users.noreply.github.com>
2024-12-03 20:52:34 -0300

    controller parte 3

--- src/main/java/br/com/bulovask/atarefados/Controller/UsuarioProjetoController.java
+++ src/main/java/br/com/bulovask/atarefados/Controller/UsuarioProjetoController.java
@@ -1,9 +1,6 @@
 package br.com.bulovask.atarefados.Controller;
 
 import br.com.bulovask.atarefados.Service.UsuarioProjetoService;
-import br.com.bulovask.atarefados.Service.UsuarioService;
-import br.com.bulovask.atarefados.entidades.Anexo;
-import br.com.bulovask.atarefados.entidades.Usuario;
 import br.com.bulovask.atarefados.entidades.UsuarioProjeto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
