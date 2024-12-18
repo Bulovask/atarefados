4ca6e6da9c905f4687dfdb105b6c16ffa7838b9f
Bulovask <ageua75@gmail.com>
2024-12-17 21:14:24 -0300

    Adicionar @JsonGetter para controlar o que mostrar

--- src/main/java/br/com/bulovask/atarefados/entidades/Lista.java
+++ src/main/java/br/com/bulovask/atarefados/entidades/Lista.java
@@ -1,8 +1,12 @@
 package br.com.bulovask.atarefados.entidades;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Entity
 @Data
@@ -14,8 +18,17 @@ public class Lista {
     private String nome;
     @ManyToOne
     @NotNull
+//    @JsonBackReference
     private Projeto projeto;
 
     public void setId(Long id) {
+    }
+
+    @JsonGetter("projeto")  // Define como a propriedade 'projeto' será serializada
+    public Map<String, Object> getProjetoInfo() {
+        Map<String, Object> projetoInfo = new HashMap<>();
+        projetoInfo.put("id", projeto.getId());
+        projetoInfo.put("nome", projeto.getNome());
+        return projetoInfo;
     }
 }
--- src/main/java/br/com/bulovask/atarefados/entidades/Projeto.java
+++ src/main/java/br/com/bulovask/atarefados/entidades/Projeto.java
@@ -1,10 +1,14 @@
 package br.com.bulovask.atarefados.entidades;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Entity
 @Data
@@ -25,4 +29,18 @@ public class Projeto {
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "projeto")
     private List<UsuarioProjeto> colaboradores;
+
+    @JsonGetter("listas")  // Define como a propriedade 'projeto' será serializada
+    public ArrayList<Map<String, Object>> getProjetoInfo() {
+
+        ArrayList<Map<String, Object>> listasInfo = new ArrayList<>();
+        for(int i = 0; i < listas.size(); i++) {
+            Lista lista = listas.get(i);
+            Map<String, Object> listaInfo = new HashMap<>();
+            listaInfo.put("id", lista.getId());
+            listaInfo.put("nome", lista.getNome());
+            listasInfo.add(listaInfo);
+        }
+        return listasInfo;
+    }
 }
